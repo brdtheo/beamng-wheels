47e84e27e2c27828122a42a038fc522f3281d676
Théo Billardey <toto.billardey@gmail.com>
2024-01-14 21:25:42 +0100

    config(next): add supabase storage URL

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'diyponmokjdkfdsflogb.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/wheels/**',
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
