2fbcf01902c59e02a33da4be590484205410addf
Théo Billardey <toto.billardey@gmail.com>
2024-01-14 20:43:11 +0100

    feat(home): import new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,56 @@
-import Image from 'next/image';
+'use client';
+
+import { useCallback } from 'react';
+
+import { useWheelList } from '@/api/wheel';
+
+import ContentWrapper from '@/components/ContentWrapper';
+import Header from '@/components/Header';
+import Pagination from '@/components/Pagination';
+
+import WheelList from '@/libs/wheel/components/WheelList';
 
 export default function Home() {
+  const { response, isError, isLoading, page, handleSetPage } = useWheelList();
+
+  const handlePreviousPage = useCallback(
+    () => handleSetPage({ endCursor: response?.pageInfo?.startCursor }),
+    [handleSetPage, response?.pageInfo?.startCursor],
+  );
+
+  const handleNextPage = useCallback(
+    () => handleSetPage({ startCursor: response?.pageInfo?.endCursor }),
+    [handleSetPage, response?.pageInfo?.endCursor],
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main>
+      <Header />
+      <ContentWrapper className="py-12">
+        <Pagination
+          totalCount={response?.totalCount ?? 0}
+          page={page}
+          hasNextPage={response?.pageInfo?.hasNextPage}
+          hasPreviousPage={response?.pageInfo?.hasPreviousPage}
+          handleNextPage={handleNextPage}
+          handlePreviousPage={handlePreviousPage}
+        />
+
+        <WheelList
+          wheelList={response?.items || []}
+          isLoading={isLoading}
+          isError={isError}
+        />
+
+        <Pagination
+          totalCount={response?.totalCount ?? 0}
+          page={page}
+          hasNextPage={response?.pageInfo?.hasNextPage}
+          hasPreviousPage={response?.pageInfo?.hasPreviousPage}
+          handleNextPage={handleNextPage}
+          handlePreviousPage={handlePreviousPage}
+        />
+      </ContentWrapper>
+    </main>
   );
 }
