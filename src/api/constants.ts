2e2c9fdd3ef0a3f36690644869aac2e121709f83
Théo Billardey <toto.billardey@gmail.com>
2024-02-11 22:59:47 +0100

    feat(api): create wheel fetcher hook
    Create a hook related to wheel list fetcher that returns page info and list items. Created associated type. Created GraphQL response parser util Fn. Created constants file.

---
+++ src/api/constants.ts
@@ -0,0 +1,4 @@
+export const API_URL = process.env.NEXT_PUBLIC_SUPABASE_API_URL || '';
+export const API_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+
+export const DEFAULT_PAGE_SIZE = 30;
